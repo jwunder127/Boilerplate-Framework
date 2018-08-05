6c5fda9ec4018bd473b9050680163788abff44a6
John Underwood <john.w.underwood.iii@gmail.com>
2018-08-05 16:24:01 -0400

    Finish backend code

--- server/api.js
+++ server/api.js
@@ -1,3 +1,37 @@
-/**
- * API Routes Module
- */
+const Router = require('koa-router');
+const database = require('./database');
+const cache = require('./cache');
+const joi = require('joi');
+const validate = require('koa-joi-validate');
+
+const router = new Router();
+
+// Check cache before continuing to any endpoint handlers
+router.use(cache.checkResponseCache);
+
+// Insert response into cache once handlers have finished
+router.use(cache.addResponseToCache);
+
+// Check that id param is valid number
+const idValidator = validate({
+  params: { id: joi.number().min(0).max(1000)
+    .required() }
+});
+
+// Check that query param is valid location type
+const typeValidator = validate({
+  params: { type: joi.string().valid(['castle', 'city', 'town', 'ruin', 'landmark', 'region']).required() }
+});
+
+// Hello World Test Endpoint
+router.get('/hello', ctx => {
+  ctx.body = 'Hello World';
+});
+
+// Get time from DB
+router.get('/time', async ctx => {
+  const result = await database.queryTime();
+  ctx.body = result;
+});
+
+module.exports = router;
--- server/database.js
+++ server/database.js
@@ -1,3 +1,20 @@
-/**
- * Postgres DB Module
- */
+const postgres = require('pg');
+const log = require('./logger');
+const connectionString = process.env.DATABASE_URL;
+
+// Initialize postgres client
+const client = new postgres.Client({ connectionString });
+
+// Connect to the DB
+client.connect().then(() => {
+  log.info(`Connected To ${client.database} at ${client.host}:${client.port}`);
+}).catch(log.error);
+
+module.exports = {
+  /** Query the current time */
+  queryTime: async () => {
+    const result = await client.query('SELECT NOW() as now');
+    return result.rows[0];
+  }
+};
+
